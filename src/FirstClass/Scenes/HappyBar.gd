b63a4fb85896728dd588ee466731824e6da955df
Giovanna Furlan Torres <giovanna.torres@sou.inteli.edu.br>
2022-02-24 11:42:26 -0300

    Update

---
+++ src/FirstClass/Scenes/HappyBar.gd
@@ -0,0 +1,8 @@
+extends HBoxContainer
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_Button_happy_pressed():
+	get_tree().change_scene("res://Cenas/Felicidade.tscn")
---
+++ src/FirstClass/Scenes/Money.gd
@@ -0,0 +1,8 @@
+extends HBoxContainer
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_Button_money1_pressed():
+	get_tree().change_scene("res://Cenas/Banco.tscn")
