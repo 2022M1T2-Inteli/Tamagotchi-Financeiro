5e1d1c991bc18f389b16d91baf368a6f8de2f016
brun0meira <99202553+brun0meira@users.noreply.github.com>
2022-02-25 09:37:42 -0300

    Update v5 - Dialog Box and phone menu
    Atualização da caixa de dialogo, do menu do celular e atualização de icons.

---
+++ src/FirstClass/Script/DetectorTeacher.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+onready var dialog = get_node("Control2/dialogBox")
+
+func _on_Detector_body_entered(body):
+	body.name == "Player_certo"
+	get_tree().change_scene("res://Scenes/dialogBoxTest.tscn")
+	#$Control2.visible = true
+
+
+func _on_Detector_body_exited(body):
+	body.name == "Player_certo"
+	#$Control2.visible = false
