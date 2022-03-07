17ca7db9b8d023b08cbe191034160c332ee8e833
ViniciosLugli <vinicioslugli@gmail.com>
2022-03-07 20:50:31 -0300

    Comment to fix code

--- src/FirstClass/Script/DetectorTeacher.gd
+++ src/FirstClass/Script/DetectorTeacher.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-onready var dialog = get_node("Control2/dialogBox")
+# onready var dialog = get_node("Control2/dialogBox") # FIXME: Node not found
 
 func _on_Detector_body_entered(body):
 	body.name == "Player_certo"
