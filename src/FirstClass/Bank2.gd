b144930e6f977508af799c362383d53169638646
Giovanna Furlan Torres <giovanna.torres@sou.inteli.edu.br>
2022-03-10 07:12:23 -0300

    Update Bank

---
+++ src/FirstClass/Bank2.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func _process(delta):
+	$Exc/AnimationPlayer.play("excla2")
+	
+func _ready():
+	pass # Replace with function body.
--- src/FirstClass/Script/erradoMercado.gd
+++ src/FirstClass/Script/erradoMercado.gd
@@ -4,4 +4,4 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_Button_errou_pressed():
-	get_tree().change_scene("res://Scenery/marketPlace.tscn")
+	get_tree().change_scene("res://Scenery/streetHouse.tscn")
