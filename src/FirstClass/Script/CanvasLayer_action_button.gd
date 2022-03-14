71bd7170b68c2c91cf0a72659d6621617b5b7892
patrickvicto <patrick.miranda@sou.inteli.edu.br>
2022-03-14 10:35:29 -0300

    interaction button

--- src/FirstClass/Script/CanvasLayer_action_button.gd
+++ src/FirstClass/Script/CanvasLayer_action_button.gd
@@ -1,10 +1,1 @@
-extends TouchScreenButton
 
-func simulate_key(which_key):
-  var ev = InputEvent()
-  ev.type = InputEvent.KEY
-  ev.scancode = which_key
-  get_tree().input_event(ev)
-
-func _on_TouchScreenButton_pressed():
-	simulate_key(16777221)
