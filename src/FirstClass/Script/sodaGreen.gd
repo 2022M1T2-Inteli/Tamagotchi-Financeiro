7512fbfbf8773536fba593b2adda4a388daaa667
Lucas Conti <lucas.pereira@sou.inteli.edu.br>
2022-03-16 16:13:51 -0300

    Audio added to the three minigames 2.0

--- src/FirstClass/Script/sodaGreen.gd
+++ src/FirstClass/Script/sodaGreen.gd
@@ -14,6 +14,7 @@ func _process(delta):
 
 func _on_Area2D_body_entered(body):
 	if (body.name == "Player_certo"):
-		queue_free()
+		$ErroFX.play()
 		jogo.errado()
 		jogo.verificar()
+		$Sprite.visible = false
