fb0ee764f4481832b3d8fa126532bf89a306026c
patrickvicto <patrick.miranda@sou.inteli.edu.br>
2022-03-17 11:08:06 -0300

    knolodge value now appers on cellphone

--- src/FirstClass/Cellphone/Knowledge.gd
+++ src/FirstClass/Cellphone/Knowledge.gd
@@ -2,12 +2,12 @@ extends Node2D
 
 # Functions for transition between knowledge status and cellphone main page
 
-func _ready():
-	pass # Replace with function body.
-
 func _on_Button_question_knowledge_pressed():
 	pass # Replace with function body.
 
 
 func _on_Button_exit_knowledge_pressed():
 	get_tree().change_scene("res://Cellphone/Status.tscn")
+
+func _ready():
+	$Knowlodge2.text = str(Global.knowledge) 
--- src/FirstClass/Cellphone/happiness.gd
+++ src/FirstClass/Cellphone/happiness.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
-# Functions for transition between happiness status and cellphone main page
+# Functions for set the visual part of happines value
 
 func _ready():
 	get_node("TextureProgress").value = Global.happiness
+
+# Functions for transition between happiness status and cellphone main page
 
 func _on_Button_question_happiness_pressed():
 	pass # Replace with function body.
