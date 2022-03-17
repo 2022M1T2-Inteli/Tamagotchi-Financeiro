65944860d131916966512a0b6b6a39100c3069ff
Lucas Conti <lucas.pereira@sou.inteli.edu.br>
2022-03-17 14:40:23 -0300

    Faculdade Sound Effects

--- src/FirstClass/Script/Acertouquiz.gd
+++ src/FirstClass/Script/Acertouquiz.gd
@@ -3,7 +3,7 @@ extends Node2D
 # Button that exit the Quiz end screen
 
 func _ready():
-	pass # Replace with function body.
+	$winfx.play()
 
 func _on_Button_exit_quiz_pressed():
 	get_tree().change_scene("res://Scenery/classUniversity.tscn")
