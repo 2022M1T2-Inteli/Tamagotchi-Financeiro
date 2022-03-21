eca434668bef21952e0e6825db574c2477441439
brun0meira <99202553+brun0meira@users.noreply.github.com>
2022-03-21 15:43:47 -0300

    Update 12.3
    Mobile update, now opens on top of the current scene.

--- src/FirstClass/Cellphone/recordsBank.gd
+++ src/FirstClass/Cellphone/recordsBank.gd
@@ -2,8 +2,11 @@ extends Node2D
 
 
 func _ready():
-	$RichTextLabel.text = str (Global.recordsPrice) 
+	if Global.recordsPrice != 0:
+		$RichTextLabel.text = str (Global.recordsPrice)
+	
+#	$RichTextLabel.text = str (Global.recordsPrice)
 
 
 func _on_Button_pressed():
-	get_tree().change_scene("res://Cellphone/Bank.tscn")
+	self.queue_free()
--- src/FirstClass/Script/Celular.gd
+++ src/FirstClass/Script/Celular.gd
@@ -1,24 +1,47 @@
 extends Node2D
 
+var celltest
+var new_node 
+
 func _ready():
 	get_node("TextureProgress").value = Global.happiness
 	$RichTextLabel.set_bbcode(str(Global.happiness) + "%")
 	$knownledge.text = str (Global.knowledge)
 
 func _on_Button_Map_pressed():
-	get_tree().change_scene("res://Cellphone/Map.tscn")
+	celltest = preload("res://Cellphone/Map.tscn")
+	new_node = celltest.instance()
+	new_node.global_position = Vector2.ZERO
+	add_child(new_node)
+	new_node.global_position = Vector2(0, 0)
+	#get_tree().change_scene("res://Cellphone/Map.tscn")
 
 
 func _on_Button_Bank_pressed():
-	get_tree().change_scene("res://Cellphone/Bank.tscn")
+	celltest = preload("res://Cellphone/Bank.tscn")
+	new_node = celltest.instance()
+	new_node.global_position = Vector2.ZERO
+	add_child(new_node)
+	new_node.global_position = Vector2(0, 0)
+	#get_tree().change_scene("res://Cellphone/Bank.tscn")
 
 
 func _on_Button_Credits_pressed():
-	get_tree().change_scene("res://Cellphone/Credits.tscn")
+	celltest = preload("res://Cellphone/Credits.tscn")
+	new_node = celltest.instance()
+	new_node.global_position = Vector2.ZERO
+	add_child(new_node)
+	new_node.global_position = Vector2(0, 0)
+	#get_tree().change_scene("res://Cellphone/Credits.tscn")
 
 
 func _on_Button_EduFin_pressed():
-	get_tree().change_scene("res://Cellphone/EduFin.tscn")
+	celltest = preload("res://Cellphone/EduFin.tscn")
+	new_node = celltest.instance()
+	new_node.global_position = Vector2.ZERO
+	add_child(new_node)
+	new_node.global_position = Vector2(0, 0)
+	#get_tree().change_scene("res://Cellphone/EduFin.tscn")
 
 
 func _on_Button_Volume_pressed():
@@ -26,20 +49,30 @@ func _on_Button_Volume_pressed():
 
 
 func _on_Button_Exit_pressed():
-	get_tree().change_scene(Global.current_scene)
+	get_tree().paused = false
+	self.queue_free()
+	#get_tree().change_scene(Global.current_scene)
 
 
 func _on_Button_exit_left_side_pressed():
-	get_tree().change_scene(Global.current_scene)
+	get_tree().paused = false
+	self.queue_free()
+	#get_tree().change_scene(Global.current_scene)
 
 
 func _on_Button_exit_right_pressed():
-	get_tree().change_scene(Global.current_scene)
+	get_tree().paused = false
+	self.queue_free()
+	#et_tree().change_scene(Global.current_scene)
 
 
 func _on_Button_Cellphone_Bottom_pressed():
-	get_tree().change_scene(Global.current_scene)
+	get_tree().paused = false
+	self.queue_free()
+	#get_tree().change_scene(Global.current_scene)
 
 
 func _on_Button_Cellphone_Top_pressed():
-	get_tree().change_scene(Global.current_scene)
+	get_tree().paused = false
+	self.queue_free()
+	#get_tree().change_scene(Global.current_scene)
