06f99bc16e62e3d9a7945f806b23ad3ea5c621d8
Filipi Enzo Siqueira Kikuchi <filipikikuchi@gmail.com>
2022-03-22 14:09:38 -0300

    Started Online Store

---
+++ src/FirstClass/Script/StoreCart.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+func _ready():
+	pass
+
+func _on_HomeRectangle_pressed():
+	get_tree().change_scene("res://Scenes/StoreHome.tscn")
+
+
+func _on_BooksRectangle_pressed():
+	get_tree().change_scene("res://Scenes/StoreBooks.tscn")
+
+
+func _on_VehiclesRectangle_pressed():
+	get_tree().change_scene("res://Scenes/StoreVehicles.tscn")
+
+
+func _on_ShirtsRectangle_pressed():
+	pass # Replace with function body.
+
+
+func _on_PetsRectangle_pressed():
+	pass # Replace with function body.
+
+
+func _on_HousesRectangle_pressed():
+	pass # Replace with function body.
---
+++ src/FirstClass/Script/StoreMenu.gd
@@ -0,0 +1,33 @@
+extends CanvasLayer
+
+
+func _ready():
+	pass
+
+func _on_Home_pressed():
+	get_tree().change_scene("res://Scenes/StoreHome.tscn")
+
+
+func _on_Books_pressed():
+	get_tree().change_scene("res://Scenes/StoreBooks.tscn")
+
+
+func _on_Vehicles_pressed():
+	get_tree().change_scene("res://Scenes/StoreVehicles.tscn")
+
+
+func _on_Shirts_pressed():
+	pass # Replace with function body.
+
+
+func _on_Pets_pressed():
+	pass # Replace with function body.
+
+
+func _on_Houses_pressed():
+	pass # Replace with function body.
+
+
+func _on_Exit_pressed():
+	Global.change_position(680,600)
+	get_tree().change_scene("res://Scenery/bedroom.tscn")
