468286be407b01d5a7a23b4b028c7b84e18e798e
patrickvicto <patrick.miranda@sou.inteli.edu.br>
2022-03-23 15:19:47 -0300

    all fix income investments are now working for the final user

--- src/FirstClass/Script/CDI_2.gd
+++ src/FirstClass/Script/CDI_2.gd
@@ -1,42 +1,51 @@
 extends Node2D
 
 func _ready():
-	pass # Replace with function body.
+	$money_invest_bank.text = str(Investiment.invest_money_cdb)
 
 func _on_Button_exit_pressed():
 	get_tree().change_scene("res://Scenes/renda_fixa.tscn")
+	
+
+func _on_Button_invest_pressed():
+	if (float(get_node("money_invest").text) >= 0) && (Global.digital_money - float(get_node("money_invest").text) >= 0):
+		Investiment.invest_money_cdb += float (get_node("money_invest").text)
+		Global.digital_money -= float (get_node("money_invest").text)
+		$money_digital.update()
+		$money_invest_bank.update()
+		$money_invest.text = ""
+	else:
+		$money_invest.text = "Valor inválido"
+		$Timer.start()
+		$money_invest.grab_focus()
+		pass # Replace with function body.
 
 
 func _on_Button_remove_pressed():
-		if (float(get_node("money_invest_4").text) >= 0) && (Global.invested_money_cdi - float(get_node("money_invest_4").text) >= 0):
-			Global.invested_money_cdi -= float (get_node("money_invest_4").text)
-			$money_invest_bank.text = str(Global.invested_money_cdi)
-			Global.digital_money += float (get_node("money_invest_4").text)
-			$money_digital.update()
-			$money_invest_4.text = ""
-		else:
-			$money_invest_4.text = "Valor inválido"
-			$Timer.start()
-			$money_invest_4.grab_focus()
-			pass # Replace with function body.
-
-
-func _on_Button_invest_pressed():
-	if (float(get_node("money_invest_4").text) >= 0) && (Global.digital_money - float(get_node("money_invest_4").text) >= 0):
-		Global.invested_money_cdi += float (get_node("money_invest_4").text)
-		$money_invest_bank.text = str(Global.invested_money_cdi)
-		Global.digital_money -= float (get_node("money_invest_4").text)
+	if (float(get_node("money_invest").text) >= 0) && (Investiment.invest_money_cdb - float(get_node("money_invest").text) >= 0) && float(get_node("money_invest").text) <= Investiment.profit_cdb:
+		Investiment.invest_money_cdb -= float (get_node("money_invest").text)
+		Global.digital_money += float (get_node("money_invest").text)
+		Investiment.profit_cdb -= float (get_node("money_invest").text)
 		$money_digital.update()
-		$money_invest_4.text = ""
-		$profit.text = str((Global.invested_money_cdi * pow(1.08,Global.day-1)) - Global.invested_money_cdi)
+		$money_invest_bank.update()
+		$profit.update()
+		$money_invest.text = ""
+	elif (float(get_node("money_invest").text) >= 0) && (Investiment.invest_money_cdb - float(get_node("money_invest").text) >= 0):
+		Investiment.invest_money_cdb -= float (get_node("money_invest").text)
+		Global.digital_money += float (get_node("money_invest").text)
+		Investiment.profit_cdb = 0
+		$money_digital.update()
+		$profit.update()
+		$money_invest_bank.update()
+		$money_invest.text = ""
 	else:
-		$money_invest_4.text = "Valor inválido"
+		$money_invest.text = "Valor inválido"
 		$Timer.start()
-		$money_invest_4.grab_focus()
-		pass # Replace with function body..
+		$money_invest.grab_focus()
+		pass # Replace with function body.
 
 
 func _on_Timer_timeout():
-	$money_invest_4.text = ""
+	$money_invest.text = ""
 	$Timer.stop()
 	pass # Replace with function body.
