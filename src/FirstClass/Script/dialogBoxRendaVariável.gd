22777c94bbcc5ab052925df0cf4ec11ac8918207
anaclaralmz <ana.zaidan@sou.inteli.edu.br>
2022-03-28 16:36:06 -0300

    update15
    quizes

--- src/FirstClass/Script/dialogBoxRendaVariável.gd
+++ src/FirstClass/Script/dialogBoxRendaVariável.gd
@@ -1,11 +1,13 @@
 extends Control
 
 var dialog = [
-	'A renda variável é como o oposto da renda fixa. Ela apresenta diversas vantagens, como: \n * Possibilidade de se obter um retorno maior mais rápido; \n * Base diária; \n * Alta liquidez; \n * Variedade grande de ativos no mercado.',
-	'Porém, ela apresenta também diversas desvantagens, como: \n\n * Valores mínimos de investimento estabelecidos; \n\n * O risco mais alto de não se obter retorno;',
-	'* A alta volatilidade dos mercados; \n\n * O fato de que o investidor entra no capital do emissor, ou seja, acaba dependendo direta ou indiretamente da valorização dos papéis da companhia no período do investimento, a qual é impossível prever de antemão.',
-	'Os tipos de investimento em renda variável são: \n Ações, Fundos Imobiliários (FIIs), ETFs, Opções, Câmbio, Futuros, Fundos de investimento e as Criptomoedas. \n\n A maneira mais conhecida de se investir nessa renda são as ações, que são a menor parcela do capital de uma empresa.',
-	'As duas formas de se lucrar com ações são: \n\n * Com a distribuição de dividendos (que são uma parte do lucro que as empresas distribuem aos acionistas); \n\n * Por meio da valorização dos papéis na bolsa de valores.'
+	'Bom dia! Você está pronto para aprender sobre Renda Variável e ações?',
+	'Ao contrário da renda fixa, o retorno das aplicações em renda variável é imprevisível no momento do investimento. \n Vantagens: \n -Possibilidade de obter um retorno maior mais rápido; \n -Alta liquidez; \n -Grande variedade de ativos no mercado.',
+	'Porém, ela apresenta algumas desvantagens, como: \n\n  -Valores mínimos de investimento estabelecidos; \n -Maior risco de obter prejuízos (pela alta volatilidade do mercado). ',
+	#'-O fato de que o investidor entra no capital do emissor, ou seja, acaba dependendo direta ou indiretamente da valorização dos papéis da companhia no período do investimento, a qual é impossível prever de antemão.',
+	'AÇÕES\n\n  A maneira mais conhecida de investir em renda variével é através da compra de ações. \n - Elas são a menor parcela do capital de uma empresa.',
+	'As duas formas de lucrar com ações são: \n\n -Distribuição de dividendos (que são uma parte do lucro que as empresas distribuem aos acionistas); \n -Por meio da valorização dos papéis na bolsa de valores.',
+	'Alguns outros exemplos de investimento em renda variável são: \n\n -Fundos Imobiliários (FIIs), ETFs, Fundos de investimento e as Criptomoedas, que serão aprofundados mais pra frente.' 
 	]
 
 
@@ -18,8 +20,8 @@ func _ready():
 func _process(delta):
 	$"Button".visible = finished
 	$Button/AnimationPlayer.play("IDLE")
-	#if Input.is_action_just_pressed("ui_accept"):
-		#load_dialog()
+	if Input.is_action_just_pressed("ui_accept"):
+		load_dialog()
 
 func load_dialog():
 	if dialog_index < dialog.size():
