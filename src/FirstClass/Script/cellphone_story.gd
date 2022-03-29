39cd5996a32b99953cbac84cbf97f6e60253ffe4
Giovanna Furlan Torres <giovanna.torres@sou.inteli.edu.br>
2022-03-29 15:55:49 -0300

    Update - Cellphone Storyline

--- src/FirstClass/Script/cellphone_story.gd
+++ src/FirstClass/Script/cellphone_story.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var time : int = 0
+
 func _ready():
 	$Story4.visible = false
 	$Story5.visible = false
@@ -7,41 +9,37 @@ func _ready():
 	$Story7.visible = false
 	$Story8.visible = false
 	$Button.visible = false
+	$Timer.start()
 	
 func _on_Timer_timeout():
-	$Story4.visible = true
-	story2()
-
-func story2():
-	$Timer2.start()
+	if time == 0 :
+		$Story4.visible = true
+		time += 1
+		$Timer2.start()
 
 func _on_Timer2_timeout():
-	$Story5.visible = true
-	story3()
-	
-func story3():
-	$Timer3.start()
+	if time == 1 :
+		$Story5.visible = true
+		time += 1
+		$Timer3.start()
 
 func _on_Timer3_timeout():
-	$Story6.visible = true
-	story4()
-
-func story4():
-	$Timer4.start()
+	if time == 2 :
+		$Story6.visible = true
+		time += 1
+		$Timer4.start()
 
 func _on_Timer4_timeout():
-	$Story7.visible = true
-	story5()
-
-func story5():
-	$Timer5.start()
+	if time == 3 :
+		$Story7.visible = true
+		time += 1
+		$Timer5.start()
 
 func _on_Timer5_timeout():
-	$Story8.visible = true
-	story6()
-
-func story6():
-	$Button.visible = true
+	if time == 4 :
+		$Story8.visible = true
+		time += 1
+		$Button.visible = true
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Scenes/taxi.tscn")
