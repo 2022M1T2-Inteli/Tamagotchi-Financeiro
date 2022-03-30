d54ae71ca012b1e57de5e9c0579a77431325be5d
patrickvicto <patrick.miranda@sou.inteli.edu.br>
2022-03-30 10:48:28 -0300

    bank instruction

--- src/FirstClass/Script/Bank_instructions.gd
+++ src/FirstClass/Script/Bank_instructions.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 func _ready():
 	if (Global.instructions[7]):
-		self.visible = false
+		self.visible = true
  #Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 	#if (Input.is_action_pressed("ui_right") || Input.is_action_pressed("ui_left") || Input.is_action_pressed("ui_up") || Input.is_action_pressed("ui_down")) || (Global.instructions[7] == false):
@@ -11,6 +11,5 @@ func _ready():
 		#call_deferred("free") # Destroy the node after the current frame.
 
 func _on_Button_pressed():
-	if (Input.is_action_pressed("ui_right") || Input.is_action_pressed("ui_left") || Input.is_action_pressed("ui_up") || Input.is_action_pressed("ui_down")) || (Global.instructions[7] == false):
-		Global.instructions[7] = false
-		call_deferred("free") # Destroy the node after the current frame
+	Global.instructions[7] = false
+	call_deferred("free") # Destroy the node after the current frame
