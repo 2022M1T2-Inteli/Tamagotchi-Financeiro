0fd4b055cb651c6770e5b5c03bbfe22e4499d4c3
MrSchipRozen <99350292+MrSchipRozen@users.noreply.github.com>
2022-03-31 10:02:21 -0300

    Update Parque
    Parque atualizado

--- src/FirstClass/Scenes/parkbalongoingupscene.gd
+++ src/FirstClass/Scenes/parkbalongoingupscene.gd
@@ -3,3 +3,10 @@ extends Node2D
 
 func _ready():
 	pass
+	$Taxi/Taxi.play("Taxii")
+	$helicopter/helicopter.play("Helicopter")
+	$Balon/Balon.play("Balon")
+	$Airplane/Airplane.play("Airplane")
+	
+func _on_Timer_timeout():
+	get_tree().change_scene("res://Scenes/Game.tscn")
