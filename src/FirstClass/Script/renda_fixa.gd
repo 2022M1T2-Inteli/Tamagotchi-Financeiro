38261192c9f436d04767431ff19a1b052c8ce93c
patrickvicto <patrick.miranda@sou.inteli.edu.br>
2022-04-04 16:14:43 -0300

    bank progression and descripition

--- src/FirstClass/Script/renda_fixa.gd
+++ src/FirstClass/Script/renda_fixa.gd
@@ -1,23 +1,29 @@
 extends Node2D
 
 func _ready():
-	pass # Replace with function body.
-
+	if Global.knowledge >= 10:
+		$FundoPreto2.visible = false
+		$FundoPreto3.visible = false
+	if Global.knowledge >= 5:
+		$FundoPreto.visible = false
 
 func _on_Button1_pressed():
 	get_tree().change_scene("res://Scenes/savings - deposit.tscn")
 
 
 func _on_Button2_pressed():
-	get_tree().change_scene("res://Scenes/CDI_2.tscn")
+	if Global.knowledge >= 5:
+		get_tree().change_scene("res://Scenes/CDI_2.tscn")
 
 
 func _on_Button3_pressed():
-	get_tree().change_scene("res://Scenes/tesouro_direto.tscn")
+	if Global.knowledge >= 10:
+		get_tree().change_scene("res://Scenes/tesouro_direto.tscn")
 
 
 func _on_Button4_pressed():
-	get_tree().change_scene("res://Scenes/debênture.tscn")
+	if Global.knowledge >= 10:
+		get_tree().change_scene("res://Scenes/debênture.tscn")
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Scenes/bank_3.tscn")
