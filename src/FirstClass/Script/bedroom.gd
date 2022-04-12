30552404b46718f6868d9d6ad340823c77738224
patrickvicto <patrick.miranda@sou.inteli.edu.br>
2022-04-12 09:39:30 -0300

    studing sytem now is working and shelf corrected

--- src/FirstClass/Script/bedroom.gd
+++ src/FirstClass/Script/bedroom.gd
@@ -65,3 +65,8 @@ func _on_Button_Instructions6_pressed():
 func _on_ComputerArea_body_entered(body):
 	if body.name == "Player_certo":
 		get_tree().change_scene("res://Scenes/StoreHome.tscn")
+
+
+func _on_shelfBooks_body_entered(body):
+	if body.name == "Player_certo":
+		get_tree().change_scene("res://Scenes/bedroomShelf.tscn")
